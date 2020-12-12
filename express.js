86d9c4aa92a265397175d1fe69e2d64864a41950
MiraeChoi <pnenv@naver.com>
2020-12-12 09:18:35 +0900

    front + back 1

--- express.js
+++ express.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const request = require("request");
 const app = express();
+const sampleApiData = require('./sample.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+//login session settings
 
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
@@ -12,6 +18,26 @@ app.get('/', function(req, res) {
 app.get('/index', function(req,res){
     res.render('index');
 });
+
+app.get('/health', function(req, res){
+    res.render('health');
+});
+
+app.get('/health_result', function(req, res){
+    res.render('health_result');
+});
+
+app.get('/inspection', (req, res)=>{
+    res.json(sampleApiData)
+})
+
+app.get('/health_recommend', (req, res)=>{
+    res.render('health_recommend')
+})
+
+app.get('/nbti_start', (req, res)=>{
+    res.render('nbti_start')
+})
 
 //잔액 조회(소연)
 app.get('/inquireBalance', function(req, res) {
